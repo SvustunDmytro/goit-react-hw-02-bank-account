61b5ecff5724d5f2abc77d06ac4b6a467092473f
SvisT <svisteckij@gmail.com>
2019-11-01 21:47:06 +0200

    homework bank acount done

---
+++ build/precache-manifest.f6c2fc77a629ce5fb65ab92efdcb3bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee9c8f6913ee33cce7bd17356525ab29",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "a2874d54c92021e0e1ad",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "d56cf81f3c260ac7ac58",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.91228c96.chunk.css"
+  },
+  {
+    "revision": "a2874d54c92021e0e1ad",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.f245091d.chunk.js"
+  },
+  {
+    "revision": "d56cf81f3c260ac7ac58",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.de160687.chunk.js"
+  },
+  {
+    "revision": "38f2ac02bb0ed3492e4a",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.cd27391c.js"
+  }
+]);
